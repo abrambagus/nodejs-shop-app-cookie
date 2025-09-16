f354127357771eb7aff077797b475844c0b6f9cb
abram <abrambagus@gmail.com>
2025-09-16 17:20:54 +0700

    deleting a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
   const isLoggedIn = req.get("Cookie");
   res.render("auth/login", {
@@ -8,6 +10,18 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect("/");
+  User.findById("68c78e92b467cb7e06ab3d38")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log("abram", err);
+    res.redirect("/login");
+  });
 };
