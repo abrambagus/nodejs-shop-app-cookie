8e2b9c959a7ba90fa5c315a50d39f74a3704d199
abram <abrambagus@gmail.com>
2025-09-16 15:40:21 +0700

    initializing session middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
+  const isLoggedIn =
+    req.get("Cookie").split(";")[0].trim().split("=")[1] === "true";
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
@@ -8,6 +9,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
+  res.setHeader("Set-Cookie", "loggedIn=true; Max-Age=3600");
   res.redirect("/");
 };
